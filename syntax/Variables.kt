c15c1e94020c9cdc9c6a3f42a4dc8a3c272c1e63
Alfredo Alas <alfredo.alas14@gmail.com>
2025-04-08 11:55:58 -0600

    add Variables.kt

---
+++ syntax/Variables.kt
@@ -0,0 +1,35 @@
+package syntax
+
+fun main () {
+
+    var  name = "Alfredo"
+
+    //Variables
+
+    //Integer
+    var age:Int = 29
+
+    //Long
+    var age2:Long = 29
+
+    //Float
+    var age3:Float = 56f
+
+    //Double
+    var age4:Double = 29.44444
+
+    //Char
+    var letra:Char = 'a'
+
+    //String
+    var nombre:String = "Alfredo Alas"
+
+    //Bool
+    var isHappy:Boolean = true
+
+    //Constans
+    val test2:String = "Alas"
+
+    test2 = "Alas2" //It is no possible
+
+}
