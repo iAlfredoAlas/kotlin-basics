ddebd9fbfacc7e171b202b6e35ab6d9a32bf1b2c
Alfredo Alas <alfredo.alas14@gmail.com>
2025-05-12 16:06:14 -0600

    add operations example

---
+++ Operations/Operations.kt
@@ -0,0 +1,30 @@
+package Operations
+
+fun main() {
+
+    //Arithmetic Operations
+
+    var age:Int = 29
+    var favoriteNumber:Int = 19
+
+    println("Sum:")
+    println(age + favoriteNumber)
+
+    println("Subtract:")
+    println(age - favoriteNumber)
+
+    println("Multiplication:")
+    println(age * favoriteNumber)
+
+    println("Division:")
+    println(age / favoriteNumber)
+
+    println("Module:")
+    println(age % favoriteNumber)
+
+    //String Operations
+
+    var name:String = "Alfredo"
+    println("My name is $name am I am $age")
+
+}
