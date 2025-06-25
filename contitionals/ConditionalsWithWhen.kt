622573349c086243575e5eb163a299f391d4f9c3
Alfredo Alas <alfredo.alas14@gmail.com>
2025-06-25 10:09:13 -0600

    add conditionals examples with when for months

---
+++ contitionals/ConditionalsWithWhen.kt
@@ -0,0 +1,23 @@
+package contitionals
+
+fun main(){
+    println (whatMonthIs(4))
+}
+
+fun whatMonthIs(month:Int):String{
+    return when (month) {
+        1 -> "January"
+        2 -> "February"
+        3 -> "March"
+        4 -> "April"
+        5 -> "May"
+        6 -> "June"
+        7 -> "July"
+        8 -> "August"
+        9 -> "September"
+        10 -> "October"
+        11 -> "November"
+        12 -> "December"
+        else -> "Is not a month"
+    }
+}
